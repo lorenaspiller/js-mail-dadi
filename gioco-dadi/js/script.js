3a73d1d1e57b8c1536cdf4ee59f7435c2d1eb4c6
Lorena Spiller <lorenaspiller@gmail.com>
2021-03-01 17:28:35 +0100

    fix exercise dice

--- gioco-dadi/js/script.js
+++ gioco-dadi/js/script.js
@@ -6,14 +6,23 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // 1. Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
 
-var giocatore = Math.floor(Math.random() * 5) + 1;
+var giocatore = Math.floor(Math.random() * 6) + 1;
 
-var computer = Math.floor(Math.random() * 5) + 1;
+var computer = Math.floor(Math.random() * 6) + 1;
 
 // 2. stabilisco il vincitore in base al punteggio più alto
 
 if (giocatore > computer) {
   document.getElementById('vincitore').innerHTML = ("giocatore");
-} else {
+  document.getElementById('numeroGiocatore').innerHTML = (giocatore);
+  document.getElementById('numeroComputer').innerHTML = (computer);
+} else if (giocatore == computer) {
+  document.getElementById('vincitore').innerHTML = ("NESSUNO!");
+  document.getElementById('numeroGiocatore').innerHTML = (giocatore);
+  document.getElementById('numeroComputer').innerHTML = (computer);
+}
+ else {
   document.getElementById('vincitore').innerHTML = ("computer");
+  document.getElementById('numeroGiocatore').innerHTML = (giocatore);
+  document.getElementById('numeroComputer').innerHTML = (computer);
 }
